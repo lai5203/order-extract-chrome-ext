c2e5c4bcc81da0dc8cef4a2a7877763ec8651a06
He Run <run.h@alibaba-inc.com>
2022-06-18 12:00:41 +0800

    add function to extract order info only without delivery details

--- src/extract/taobao.js
+++ src/extract/taobao.js
@@ -8,100 +8,109 @@ chrome.runtime.onMessage.addListener(
         if (request.command == "stop"){
             console.log("Received stop signal");
             stopRun = true;
-        }else if(request.command == "extract"){
-            try{
-                stopRun = false;
-                let result = "";
-                let orderRoot = document.getElementById("tp-bought-root");
-                let orderElements = orderRoot.getElementsByClassName("js-order-container");
-                // let orderElements = orderRoot.getElementsByClassName("index-mod__order-container___1ur4- js-order-container");
-
-                if (orderElements.length == 0){
-                    message = "当前页面没有找到订单";
-                    sendResponse({message: message, success: false});
-                    return;
-                }
-
-
-                let processedCount = 0;
-
-                for (let index = 0; index < orderElements.length && !stopRun; index++ ){
-                    processedCount++;
-                    let bodies = orderElements[index].getElementsByTagName("tbody");
-                    let orderId = bodies[0].getElementsByTagName("td")[0].textContent;
-                    let orderDate = orderId.substring(0,10);
-                    orderId = orderId.substring(orderId.indexOf("订单号")+4);
-
-                    let itemRows = bodies[1].getElementsByTagName("tr");
-                    let cells = itemRows[0].getElementsByTagName("td");
-                    let itemName = cells[0].innerText;
-                    let cutIndex = itemName.indexOf("[交易快照]");
-                    if (cutIndex < 0){
-                        cutIndex = itemName.indexOf("\n");
-                    }
-
-                    if (cutIndex > 0){
-                        itemName = itemName.substring(0, cutIndex);
-                    }
-
-                    let remainingItemsCount = itemRows.length - 1;
-
-                    if (remainingItemsCount > 0){
-                        if (itemRows[itemRows.length - 1].getElementsByTagName("td")[0].innerText == "保险服务"){
-                            remainingItemsCount--;
-                        }
-
-                        if (remainingItemsCount > 0){
-                            itemName = itemName + " --及其他" + (itemRows.length - 2)+"件物品"
-                        }
-                    }
-
-                    chrome.runtime.sendMessage({item: itemName, orderId: orderId, event: "processItem"});
-
-                    let deliveryStatus = "";
-                    var priceEle = cells[4].getElementsByTagName('strong');
-                    let paidAmount = "获取失败";
-                    if (priceEle != null){
-                        paidAmount = priceEle[0].textContent.replace("￥","");
-                    }
-
-                    let shortTransportLink = "https://buyertrade.taobao.com/trade/json/transit_step.do?bizOrderId=" + orderId
-                    // let shortTransportLink = "https://detail.i56.taobao.com/call/queryTrace.do?dimension=TRADE_ID&tradeId=" + orderId;
-
-                    let transportResponse = getSyncUrlResponse(shortTransportLink);
-
-                    if (transportResponse.indexOf("签收") > 0){
-                        deliveryStatus = "已签收";
-                    }
-
-                    if (request.debug){
-                        console.log("transport response: ", transportResponse)
-                    }
-
-                    transportResponse = JSON.parse(transportResponse);
-
-                    let expressName = transportResponse["expressName"] ?? "无记录";
-                    let expressId = transportResponse["expressId"] ?? "无记录";
-
-                    result = result + orderDate + "\t" + itemName + "\t" + paidAmount + "\t" + expressName + "\t" + expressId + "\t" + deliveryStatus + "\n";
-                }
-
-                copyTextToClipboard(result);
-                if (request.debug){
-                    console.log(result)
-                }
-                message = "已处理" + processedCount+ "条记录，一共"+orderElements.length+"条。";
-                sendResponse({message: message, success: true});
-            }catch (e) {
-                console.error(e);
-                message = "未知错误";
-                sendResponse({message: message, success: false});
-            }
-
-
+        }else if(request.command == "extract-order-delivery"){
+            extract(request, sendResponse, true);
+        }else if(request.command == "extract-order"){
+            extract(request, sendResponse, false);
         }
     }
 );
+
+function extract(request, sendResponse, includeDelivery){
+    try{
+        stopRun = false;
+        let result = "";
+        let orderRoot = document.getElementById("tp-bought-root");
+        let orderElements = orderRoot.getElementsByClassName("js-order-container");
+        // let orderElements = orderRoot.getElementsByClassName("index-mod__order-container___1ur4- js-order-container");
+
+        if (orderElements.length == 0){
+            message = "当前页面没有找到订单";
+            sendResponse({message: message, success: false});
+            return;
+        }
+
+
+        let processedCount = 0;
+
+        for (let index = 0; index < orderElements.length && !stopRun; index++ ){
+            processedCount++;
+            let bodies = orderElements[index].getElementsByTagName("tbody");
+            let orderId = bodies[0].getElementsByTagName("td")[0].textContent;
+            let orderDate = orderId.substring(0,10);
+            orderId = orderId.substring(orderId.indexOf("订单号")+4);
+
+            let itemRows = bodies[1].getElementsByTagName("tr");
+            let cells = itemRows[0].getElementsByTagName("td");
+            let itemName = cells[0].innerText;
+            let cutIndex = itemName.indexOf("[交易快照]");
+            if (cutIndex < 0){
+                cutIndex = itemName.indexOf("\n");
+            }
+
+            if (cutIndex > 0){
+                itemName = itemName.substring(0, cutIndex);
+            }
+
+            let remainingItemsCount = itemRows.length - 1;
+
+            if (remainingItemsCount > 0){
+                if (itemRows[itemRows.length - 1].getElementsByTagName("td")[0].innerText == "保险服务"){
+                    remainingItemsCount--;
+                }
+
+                if (remainingItemsCount > 0){
+                    itemName = itemName + " --及其他" + (itemRows.length - 2)+"件物品"
+                }
+            }
+
+            chrome.runtime.sendMessage({item: itemName, orderId: orderId, event: "processItem"});
+
+            let deliveryStatus = "";
+            var priceEle = cells[4].getElementsByTagName('strong');
+            let paidAmount = "获取失败";
+            if (priceEle != null){
+                paidAmount = priceEle[0].textContent.replace("￥","");
+            }
+
+            if (includeDelivery){
+                let shortTransportLink = "https://buyertrade.taobao.com/trade/json/transit_step.do?bizOrderId=" + orderId
+                // let shortTransportLink = "https://detail.i56.taobao.com/call/queryTrace.do?dimension=TRADE_ID&tradeId=" + orderId;
+    
+                let transportResponse = getSyncUrlResponse(shortTransportLink);
+    
+                if (transportResponse.indexOf("签收") > 0){
+                    deliveryStatus = "已签收";
+                }
+    
+                if (request.debug){
+                    console.log("transport response: ", transportResponse)
+                }
+    
+                transportResponse = JSON.parse(transportResponse);
+    
+                let expressName = transportResponse["expressName"] ?? "无记录";
+                let expressId = transportResponse["expressId"] ?? "无记录";
+    
+                result = result + orderDate + "\t" + itemName + "\t" + paidAmount + "\t" + expressName + "\t" + expressId + "\t" + deliveryStatus + "\n";
+            }else{
+                result = result + orderDate + "\t" + itemName + "\t" + paidAmount + "\t" + (0-paidAmount) + "\n";
+            }
+            
+        }
+
+        copyTextToClipboard(result);
+        if (request.debug){
+            console.log(result)
+        }
+        message = "已处理" + processedCount+ "条记录，一共"+orderElements.length+"条。";
+        sendResponse({message: message, success: true});
+    }catch (e) {
+        console.error(e);
+        message = "未知错误";
+        sendResponse({message: message, success: false});
+    }
+}
 
 function getSyncUrlResponse(url){
     var xhr = new XMLHttpRequest();
--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,87 @@
 'use strict';
 
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.event == "processItem"){
+            document.getElementById('display').innerText = request.display;
+        }
+
+        sendResponse({success: true});
+    });
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var extractOrdersBtn = document.getElementById('extractOrders');
+        extractOrdersBtn.addEventListener('click', function() {
+            extractOrders();
+        }, false);
+
+        var extractOrderAndDeliveryBtn = document.getElementById('extractOrderAndDelivery');
+        extractOrderAndDeliveryBtn.addEventListener('click', function() {
+            extractOrderAndDelivery();
+        }, false);
+
+      }, false);
+    
+
+function extractOrders(){
+    console.log("extractOrders");
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var activeTab = tabs[0];
+        console.log(activeTab.url);
+
+        startProcessing();
+
+        chrome.tabs.sendMessage(activeTab.id, {"command": "extract-order", "debug": false},function(response) {
+            if (response.success){
+                endProcessing();
+    
+                setTimeout(function () {
+                    alert('订单信息（不包含物流）已经复制到粘贴板，您可以到Excel等应用中粘贴。'+ response.message);
+                }, 500);
+            }else{
+                alert('订单获取失败。提示信息：' + response.message);
+            }
+        });
+    });
+}
+
+
+function extractOrderAndDelivery(){
+    console.log("extractOrderAndDelivery");
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var activeTab = tabs[0];
+        console.log(activeTab.url);
+
+        startProcessing();
+
+        chrome.tabs.sendMessage(activeTab.id, {"command": "extract-order-delivery", "debug": false},function(response) {
+            if (response.success){
+                endProcessing();
+    
+                setTimeout(function () {
+                    alert('订单详情已经复制到粘贴板，您可以到Excel等应用中粘贴。由于限制，如果一个订单有多个包裹，只能自动获取到一个，请手动获取其他快递信息。'+ response.message);
+                }, 500);
+            }else{
+                alert('订单获取失败。提示信息：' + response.message);
+            }
+        });
+    });
+}
+
+function startProcessing(){
+    document.getElementById('loading').style.display ='block';
+    document.getElementById('done').style.display ='none';
+    document.getElementById('init').style.display ='none';
+}
+
+function endProcessing(){
+    document.getElementById('loading').style.display ='none';
+    document.getElementById('done').style.display ='block';
+    document.getElementById('init').style.display ='none';
+}
+
 /*let stopButton = document.getElementById('stopProcessBtn');
 
 stopButton.onclick = function(element) {
@@ -9,25 +91,6 @@ stopButton.onclick = function(element) {
     });
 };*/
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    document.getElementById('loading').style.display ='block';
-    document.getElementById('done').style.display ='none';
-    document.getElementById('init').style.display ='none';
-    chrome.tabs.sendMessage(activeTab.id, {"command": "extract", "debug": false},function(response) {
-        if (response.success){
-            document.getElementById('loading').style.display ='none';
-            document.getElementById('done').style.display ='block';
-            document.getElementById('init').style.display ='none';
-
-            setTimeout(function () {
-                alert('订单详情已经复制到粘贴板，您可以到Excel等应用中粘贴。由于限制，如果一个订单有多个包裹，只能自动获取到一个，请手动获取其他快递信息。'+ response.message);
-            }, 500);
-        }else{
-            alert('订单获取失败。提示信息：' + response.message);
-        }
-    });
-});
 
 
 chrome.runtime.onMessage.addListener(
